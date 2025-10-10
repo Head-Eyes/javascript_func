cfce670d79adb8d105a0ae4e77dcc967f815bb52
Gyimesi Gergo Mark <gyimesi.gergo@BOLYAI.LOCAL>
2025-10-10 08:56:17 +0200

    js_if

---
+++ table_row/kitero.js
@@ -0,0 +1,28 @@
+const vmi = {}
+const szam = 8
+
+
+if (szam > 5)
+{
+    vmi.name = "BAAAAH"
+    
+}
+else{
+    vmi["name"] = "NAAAAAH"    
+}
+console.log(vmi)
+console.log(vmi.name)
+
+const szam2 = 6
+
+if (vmi.age == undefined) // if(vmi.age)
+{
+
+}
+
+/*
+if (vmi.age == null) --> igaz
+{}
+if (vmi.age === null) --> hamis
+{}
+*/
